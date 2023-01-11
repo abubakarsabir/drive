ff388a6957953f8a7491d22f2ecef7b846cfabca
abubakar sabir <abubakarsabir666@gmail.com>
2023-01-11 08:26:12 +0500

    fixed issues with feature file and locator

--- cypress/e2e/features/filters.feature
+++ cypress/e2e/features/filters.feature
@@ -3,29 +3,15 @@ Feature: Shop Car Filters
   As a Registered user
   I want to specify the features of shop car filters
 
-  Scenario: 
+   Background:
     Given 85301 is the zip code
+
+  Scenario: condition filter testing 
     When The user selects the ShopCars tab
     Then The user tries various "<options>" within the condition tab
 
-  #Scenario: GitHub Bootcamp Section
-   # Given user is on GitHub home page
-   # When user focuses on GitHub Bootcamp Section
-   # Then user gets an option to setup git
-   # And user gets an option to create repository
-   # And user gets an option to Fork Repository
-   # And user gets an option to work together
-
-  #Scenario: Top Banner content
-   # Given user is on GitHub home page
-   # When user focuses on Top Banner
-   # Then user gets an option of home page
-   # And user gets an option to search
-   # And user gets settings options
-   # And user gets an option to logout
-
    Examples:
-                |options |
-                |New   |
-                | Used |   
-                | Certified Pre-owned |
+     |options|
+     |New    |
+     |Used   |   
+     |Certified Pre-owned |
--- cypress/pages/ShopCarsPage.js
+++ cypress/pages/ShopCarsPage.js
@@ -1,19 +1,28 @@
 class ShopCarsPage {
-    elements = {
-        //verifying the zip code locator
-        zipCode: () => cy.get('p[class="font-abc-repro text-evergreen-100 mx-2 font-medium text-focus"]'),
-        shopCarsbtn: () => cy.contains('p', 'Shop Cars'),
-        headerText: () => cy.contains('h6', 'Get your rate and customize your terms.'),
-        //condtion tab locators
-        conditionTab: () => cy.get('h6').contains('Condition'),
-        newCheckBox: () => cy.get('div[class="flex items-start"]').contains('New'),
-        showCarsBtn: () => cy.get('footer[class="border-t-1 border-neutral-gum flex items-center justify-between border-t p-4 pb-6"]')
-            .find('p[class="font-abc-repro select-none"]'),
-        usedCheckBox: () => cy.get('div[class="flex items-start"]').contains('Used'),
-        certifiedCheckBox: () => cy.get('div[class="flex items-start"]').contains('Certified Pre-owned'),
-
-
-    };
+  elements = {
+    zipCode: () =>
+      cy
+        .get(".group.flex.grow.cursor-pointer.items-center.justify-end")
+        .find(
+          'p[class="font-abc-repro text-evergreen-100 mx-2 font-medium text-focus"]'
+        ),
+    shopCarsbtn: () => cy.contains("p", "Shop Cars"),
+    headerText: () =>
+      cy.contains("h6", "Get your rate and customize your terms."),
+    //condtion tab locators
+    conditionTab: () => cy.get("h6").contains("Condition"),
+    newCheckBox: () => cy.get('div[class="flex items-start"]').contains("New"),
+    showCarsBtn: () =>
+      cy
+        .get(
+          'footer[class="border-t-1 border-neutral-gum flex items-center justify-between border-t p-4 pb-6"]'
+        )
+        .find('p[class="font-abc-repro select-none"]'),
+    usedCheckBox: () =>
+      cy.get('div[class="flex items-start"]').contains("Used"),
+    certifiedCheckBox: () =>
+      cy.get('div[class="flex items-start"]').contains("Certified Pre-owned"),
+  };
 }
 
 export const scp = new ShopCarsPage();
